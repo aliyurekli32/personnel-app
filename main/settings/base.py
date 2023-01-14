605db0ff289df5000190a5025a1fe8eb13ed5604
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-14 13:03:41 +0300

    update: main

--- main/settings/base.py
+++ main/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'dj_rest_auth',
     'users',
+    'personnel',
 
 ]
 
--- main/urls.py
+++ main/urls.py
@@ -38,6 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
+    path("api/", include("personnel.urls")),
     # Url paths for swagger:
     path("swagger(<format>\.json|\.yaml)",schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui"),
