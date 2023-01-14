fc188231d5a835b90fd800183eb4795c25a36245
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-14 13:03:18 +0300

    create: serializers

---
+++ personnel/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Department, Personnel
+from django.utils import timezone
+
+class DepartmetSerializer(serializers.ModelSerializer):
+    
+    personnel_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Department
+        fields =("id", "name", "personnel_count") #"__all__"
+    
+    def get_personnel_count(self, obj):
+        return obj.personals.count()
+    
+class PersonnelSerializer(serializers.ModelSerializer):
+    days_since_joined=serializers.SerializerMethodField()
+    create_user_id = serializers.IntegerField(required=False)
+    create_user = serializers.StringRelatedField()
+    
+    class Meta:
+        model: Personnel
+        fields = "__all__"
+    
+    def create(self, validated_data):
+        validated_data["create_user_id"] = self.context['request'].user.id 
+        instance = Personnel.objects.create(**validated_data)
+        return instance
+    def get_days_since_joined(self,obj):
+        return (now() - obj.start)
