68c2eda8d3c5b198be382645b5f2c76bc1354b54
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-15 23:21:00 +0300

    update viewspy

--- personnel/views.py
+++ personnel/views.py
@@ -2,9 +2,9 @@ import statistics
 from django.shortcuts import render
 from requests import Response
 from .models import Department, Personnel
-from .serializers import DepartmetSerializer,PersonnelSerializer
+from .serializers import DepartmetSerializer, DerpartmentPersonnelSerializer,PersonnelSerializer
 from rest_framework import generics,permissions
-from .permissions import IsStafforReadOnly
+from .permissions import IsStafforReadOnly,IsOwnerAndStafforReadOnly
 
 
 # Create your views here.
@@ -18,7 +18,7 @@ class PersonnelListCreateView(generics.ListCreateAPIView):
     serializer_class = PersonnelSerializer
     queryset = Personnel.objects.all()
     # permission_classes = [IsAuthenticated, IsStafforReadOnly]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -43,3 +43,39 @@ class PersonnelListCreateView(generics.ListCreateAPIView):
         person.create_user = self.request.user
         person.save()
         return person
+    
+
+
+class PersonalGetUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Personnel.objects.all()
+    serializer_class=PersonnelSerializer
+    # permission_classes = [IsAuthenticated,IsOwnerAndStafOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
+
+    
+    def put(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if self.request.user.is_staff and (instance.create_user == self.request.user):
+            return self.update(request, *args, **kwargs)
+        else:
+            data = {
+                "message": "Yetkiniz yoktur.."
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+    
+    def delete(self,request,*args,**kwargs):
+        if self.request.user.is_superuser:
+            return self.destroy(request,*args,**kwargs)
+        else:
+            data = {
+                "message": "Yetkiniz yoktur.."
+            }
+            return Response(data ,status=status.HTTP_401_UNAUTHORIZED)
+        
+class DepartmentPersonnelView(generics.ListAPIView):
+    serializer_class = DerpartmentPersonnelSerializer
+    queryset = Department.objects.all()
+    
+    def get_queryset(self):
+        name = self.kwargs["department"]
+        return Department.objects.filter(name_ieact=name)
