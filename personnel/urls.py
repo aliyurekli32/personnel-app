082d640b0fa8bfaadae7e7a5792fdf86bc7bd2ac
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-15 23:20:46 +0300

    update urls.py

--- personnel/urls.py
+++ personnel/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import DepartmentView,PersonnelListCreateView
+from .views import DepartmentPersonnelView, DepartmentView, PersonnelListCreateView, PersonalGetUpdateDelete
 
 urlpatterns = [
     path("department/", DepartmentView.as_view()),
-    path("personnel",PersonnelListCreateView.as_view()),
+    path("personnel/", PersonnelListCreateView.as_view()),
+    path("personnel/<int:pk>/",PersonalGetUpdateDelete.as_view()),
+    path("personnel/<str:department>/",DepartmentPersonnelView.as_view()),
 ]
