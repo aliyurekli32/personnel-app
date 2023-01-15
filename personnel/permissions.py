5c5c08675420f54fde354b79814772e2024397b6
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-15 23:12:50 +0300

    update: permissions

--- personnel/permissions.py
+++ personnel/permissions.py
@@ -8,3 +8,9 @@ class IsStafforReadOnly(permissions.IsAdminUser):
             return True
         else:
             return bool(request.user and request.user.is_staff)
+        
+class IsOwnerAndStafforReadOnly(permissions.BasePermission):
+   def has_object_permissions(self, request, view, obj):
+       if request.method in permissions.SAFE_METHODS:
+           return True
+       return bool(request.user.is_staff and (obj.create_user==request.user))
